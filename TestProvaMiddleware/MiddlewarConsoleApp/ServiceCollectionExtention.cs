d3c99de77d82d3aaaed986993e8e627f17aa78e5
Pippi <141743043+PippiF@users.noreply.github.com>
2023-10-18 14:44:49 +0200

    Creazione TestMiddleware

---
+++ TestProvaMiddleware/MiddlewarConsoleApp/ServiceCollectionExtention.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddlewarConsoleApp
+{
+    public static class ServiceCollectionExtention
+    {
+        public static void ConfigureMiddle(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<MyMiddlewareProva>();
+        }
+    }
+}
---
+++ TestProvaMiddleware/TestProvaMiddleware/Program.cs
@@ -0,0 +1,36 @@
+using Serilog;
+using TestProvaMiddleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+
+//Log.Logger = new LoggerConfiguration()
+//        //.WriteTo.File(new CompactJsonFormatter(), "log.txt")
+//        .CreateLogger();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.ConfigureMiddl();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
